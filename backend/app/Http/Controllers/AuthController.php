b6c9d3e87614cb0e58a1f073ab66cd125907e808
davydocsurg <davydocsurg@gmail.com>
2023-03-03 13:54:44 +0100

    install and setup Laravel Passport; create AuthController #2

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+}
