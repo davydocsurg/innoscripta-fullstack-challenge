86cf26d6e51de3d5703aec5d91c08c19d6ddaea2
davydocsurg <davydocsurg@gmail.com>
2023-03-03 14:09:42 +0100

    add user registration method #2

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 class AuthController extends Controller
 {
     /**
@@ -12,5 +17,63 @@ class AuthController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Register a new user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validate = $this->validator($request);
+        if ($validate->fails()) {
+            return response([
+                'status' => false,
+                'errors' => $validate->errors()->messages(),
+            ], 400);
+        }
+
+        $user = new User();
+        $this->store($request, $user);
+
+        return response([
+            'status' => true,
+            'message' => 'Registeration was successfully',
+        ], 201);
+    }
+
+    /**
+     * User data validator
+     * @param Request $request
+     * @param array $customRules
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public function validator(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|unique:users|email:filter,rfc,dns|string|max:255',
+            "password" => "required|string|min:8|confirmed",
+        ]);
+    }
+
+    /**
+     * Store newly created resource
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store($request, $user)
+    {
+        $user->first_name = ucfirst($request->first_name);
+        $user->last_name = ucfirst($request->last_name);
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
     }
 }
