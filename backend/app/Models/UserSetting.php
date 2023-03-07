464a667fbd7f02a8be90f376614aaaed1b2e9086
davydocsurg <davydocsurg@gmail.com>
2023-03-07 09:00:59 +0100

    create user settings table; define relationships #4

---
+++ backend/app/Models/UserSetting.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserSetting extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'key',
+        'settings',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'settings' => 'array',
+    ];
+
+    /**
+     * Get the user that owns the setting.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
