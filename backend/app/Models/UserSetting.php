02b4027e986fe4ffb276f934099b38e1ebafb9f9
davydocsurg <davydocsurg@gmail.com>
2023-03-07 09:12:14 +0100

    add user settings methods #4

--- backend/app/Models/UserSetting.php
+++ backend/app/Models/UserSetting.php
@@ -9,6 +9,12 @@ class UserSetting extends Model
 {
     use HasFactory;
 
+    const DEFAULT_SETTINGS_ARRAY = [
+        'favorite_categories' => [],
+        'favorite_sources' => [],
+        'favorite_authors' => [],
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -37,4 +43,30 @@ class UserSetting extends Model
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Get the user's settings.
+     */
+    public static function settings($user_id)
+    {
+        return UserSetting::where('user_id', $user_id)->get();
+    }
+
+    /**
+     * Fetch default settings.
+     */
+    public static function defaultSettings($encoded = true)
+    {
+        if ($encoded) {
+            return json_encode(self::DEFAULT_SETTINGS_ARRAY);
+        }
+        return self::DEFAULT_SETTINGS_ARRAY;
+    }
+
+    /**
+     * Fetch default settings as an object
+     */
+    public static function formattedSettings($user)
+    {
+        return json_decode($user->attributes['settings']);
+    }
 }
